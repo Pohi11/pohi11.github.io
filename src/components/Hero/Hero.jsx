dc51b8d7d1a42ccde87c40d7b9520f75a4a96645
Pohi11 <dahingo69@gmail.com>
2024-07-27 17:40:24 -0400

    initial commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Jonathan</h1>
+        <p className={styles.description}>
+          I am a computer science student specialized in Cybersecurity and
+          enthusiast about different cloud platforms. Keep reading to learn
+          more!
+        </p>
+        <a
+          href="mailto:jonathandorfman@cmail.carleton.ca"
+          className={styles.contactBtn}
+        >
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Photo"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
